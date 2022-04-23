50ad75c0457f2409509d57ffb22f3ec009ca958b
robertosixtyone <robertohermenegildodias@gmail.com>
2022-04-23 10:39:36 -0300

    Add OP_MINUS

--- src/no.go
+++ src/no.go
@@ -8,6 +8,10 @@ var programm = []Op{
 	{op: OP_PUSH_INT, operand: 34},
 	{op: OP_PUSH_INT, operand: 35},
 	{op: OP_PLUS},
+	{op: OP_PUSH_INT, operand: 34},
+	{op: OP_MINUS},
+	{op: OP_PUSH_INT, operand: 34},
+	{op: OP_PLUS},
 	{op: OP_PRINT},
 }
 
