63cdbcfbb3f77f9e036c861568815bff36356a74
robertosixtyone <robertohermenegildodias@gmail.com>
2022-04-27 14:44:01 -0300

    Add operations that store and load 16 bits from the memory

--- src/compiler.go
+++ src/compiler.go
@@ -34,6 +34,8 @@ const (
 	OP_MEM      OpType = iota
 	OP_LOAD8    OpType = iota
 	OP_STORE8   OpType = iota
+	OP_LOAD16   OpType = iota
+	OP_STORE16  OpType = iota
 
 	OP_COUNT    OpType = iota
 )
@@ -52,7 +54,7 @@ func isError(err error) bool {
 }
 
 func generateYasmLinux_x86_64(program []Op, output string) {
-	if !(OP_COUNT == 17) {
+	if !(OP_COUNT == 19) {
 		fmt.Fprintf(os.Stderr, "Assertion Failed: Exhaustive handling of ops in generateYasmLinux_x86_64\n")
 		os.Exit(1)
 	}
@@ -208,6 +210,17 @@ func generateYasmLinux_x86_64(program []Op, output string) {
 			f.WriteString("    pop rax\n"           )
 			f.WriteString("    pop rbx\n"           )
 			f.WriteString("    mov [rax], bl\n"     )
+		case OP_LOAD16:
+			f.WriteString("    ;; -- load16 --\n"   )
+			f.WriteString("    pop rax\n"           )
+			f.WriteString("    xor rbx, rbx\n"      )
+			f.WriteString("    mov bx, [rax]\n"     )
+			f.WriteString("    push rbx\n"          )
+		case OP_STORE16:
+			f.WriteString("    ;; -- store16 --\n"  )
+			f.WriteString("    pop rax\n"           )
+			f.WriteString("    pop rbx\n"           )
+			f.WriteString("    mov [rax], bx\n"     )
 		default:
 			fmt.Fprintf(os.Stderr, "ERROR: Unreachable\n")
 			os.Exit(2)
@@ -241,7 +254,7 @@ func compileTokensIntoOps(tokens []Token) []Op {
 		case TOKEN_INT:
 			ops = append(ops, Op{op: OP_PUSH_INT, operand: Operand(token.icontent)})
 		case TOKEN_WORD:
-			if !(OP_COUNT == 17) {
+			if !(OP_COUNT == 19) {
 				fmt.Fprintf(os.Stderr, "Assertion Failed: Exhaustive handling of ops in compileTokensIntoOps\n")
 				os.Exit(1)
 			}
@@ -279,6 +292,10 @@ func compileTokensIntoOps(tokens []Token) []Op {
 				ops = append(ops, Op{op: OP_LOAD8})
 			case token.scontent == "!8":
 				ops = append(ops, Op{op: OP_STORE8})
+			case token.scontent == "@16":
+				ops = append(ops, Op{op: OP_LOAD16})
+			case token.scontent == "!16":
+				ops = append(ops, Op{op: OP_STORE16})
 			default:
 				loc := token.loc
 				fmt.Fprintf(os.Stderr, "%s:%d:%d: ERROR: Unknown word: %s\n", loc.f, loc.r, loc.c,
