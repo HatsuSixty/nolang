2d56a783cb4b01a5eee2f430c0dddc52f6412f2f
robertosixty1 <roberto011herminegildo@gmail.com>
2022-05-04 15:21:40 -0300

    Add `2swap` instruction

--- src/compiler.go
+++ src/compiler.go
@@ -47,6 +47,7 @@ const (
 	OP_OVER     OpType = iota
 	OP_ROT      OpType = iota
 	OP_2DUP     OpType = iota
+	OP_2SWAP    OpType = iota
 
 	// logic (booleans)
 	OP_EQ       OpType = iota
@@ -74,7 +75,7 @@ type Op struct {
 }
 
 func generateYasmLinux_x86_64(program []Op, output string) {
-	if !(OP_COUNT == 39) {
+	if !(OP_COUNT == 40) {
 		fmt.Fprintf(os.Stderr, "Assertion Failed: Exhaustive handling of ops in generateYasmLinux_x86_64\n")
 		os.Exit(1)
 	}
@@ -375,6 +376,16 @@ func generateYasmLinux_x86_64(program []Op, output string) {
 			f.WriteString("    push rax\n"           )
 			f.WriteString("    push rbx\n"           )
 			f.WriteString("    push rax\n"           )
+		case OP_2SWAP:
+			f.WriteString("    ;; -- 2swap --\n"     )
+			f.WriteString("    pop rax\n"            )
+			f.WriteString("    pop rbx\n"            )
+			f.WriteString("    pop rcx\n"            )
+			f.WriteString("    pop rdx\n"            )
+			f.WriteString("    push rax\n"           )
+			f.WriteString("    push rbx\n"           )
+			f.WriteString("    push rcx\n"           )
+			f.WriteString("    push rdx\n"           )
 		default:
 			fmt.Fprintf(os.Stderr, "ERROR: Unreachable\n")
 			os.Exit(2)
@@ -396,11 +407,11 @@ func generateYasmLinux_x86_64(program []Op, output string) {
 func crossreferenceBlocks(program []Op) []Op {
 	mprogram := program
 	var stack []int
-	var ifIp int
+	var ifIp    int
 	var blockIp int
 	var whileIp int
 	for i := range mprogram {
-		if !(OP_COUNT == 39) {
+		if !(OP_COUNT == 40) {
 			fmt.Fprintf(os.Stderr, "Assertion Failed: Exhaustive handling of ops in crossreferenceBlocks. Add here only operations that form blocks\n")
 			os.Exit(1)
 		}
@@ -489,7 +500,7 @@ func compileTokensIntoOps(tokens []Token) []Op {
 		case TOKEN_INT:
 			ops = append(ops, Op{op: OP_PUSH_INT, operand: Operand(token.icontent), loc: token.loc})
 		case TOKEN_WORD:
-			if !(OP_COUNT == 39) {
+			if !(OP_COUNT == 40) {
 				fmt.Fprintf(os.Stderr, "Assertion Failed: Exhaustive handling of ops in compileTokensIntoOps\n")
 				os.Exit(1)
 			}
@@ -571,6 +582,8 @@ func compileTokensIntoOps(tokens []Token) []Op {
 				ops = append(ops, Op{op: OP_ROT, loc: token.loc})
 			case token.scontent == "2dup":
 				ops = append(ops, Op{op: OP_2DUP, loc: token.loc})
+			case token.scontent == "2swap":
+				ops = append(ops, Op{op: OP_2SWAP, loc: token.loc})
 			default:
 				loc := token.loc
 				fmt.Fprintf(os.Stderr, "%s:%d:%d: ERROR: Unknown word: %s\n", loc.f, loc.r, loc.c,
